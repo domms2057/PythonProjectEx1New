d84adaeab3dabbca9dcdcba2e45df6c741f84819
dommz <domiziano.myers@gmail.com>
2025-01-23 18:57:01 +0100

    Ex.1

---
+++ Ex.1.py
@@ -0,0 +1,9 @@
+person = {'name': 'Dom', 'age': 19, 'city': 'New York'}
+print("Name:", person['name'])
+print("Age:", person['age'])
+print("Thoughts: The weather has gotten considerably worse in Madrid")
+print("Thoughts: How much would I spend on a JBL compared to a Bose speaker and how much would I spend on two speakers in total?")
+JBL = 150
+Bose = 200
+sum = JBL + Bose
+print("The sum of what I would spend on two speakers is:", sum)
